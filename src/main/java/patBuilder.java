8074a95d1fe22401c3403db5293ab4dd88dedd96
675710 <jesse.kruse@student.uni-luebeck.de>
2020-11-28 13:41:02 +0100

    jason für KH

---
+++ src/main/java/patBuilder.java
@@ -0,0 +1,90 @@
+import ca.uhn.fhir.context.FhirContext;
+import org.hl7.fhir.r4.model.*;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Random;
+
+public class patBuilder {
+    FhirContext ctx;
+
+    public patBuilder(FhirContext ctx)  {
+        this.ctx = ctx;
+    }
+
+    public void buildPatient()    {
+
+        Patient pat = new Patient();
+
+        Identifier id = pat.addIdentifier();
+        id.setSystem("http://www.kh-uzl.de/fhir/patients");
+        int patIdVal = new Random().nextInt(10000);
+        id.setValue(String.valueOf(patIdVal));
+
+        // set name
+        HumanName name = pat.addName();
+        name.setUse(HumanName.NameUse.OFFICIAL);
+        name.setFamily("Grünlichr");
+        name.addGiven("Antonie");
+
+        //set maiden name
+        HumanName maidenName = pat.addName();
+        maidenName.setUse(HumanName.NameUse.MAIDEN);
+        maidenName.setFamily("Bruddenbrooks");
+
+        // set Birthdate
+        pat.setBirthDateElement(new DateType("1827-08-06"));
+
+        // set marital Status
+        Coding maritalCoding = new Coding();
+        maritalCoding.setSystem("http://terminology.hl7.org/CodeSystem/v3-MaritalStatus");
+        maritalCoding.setCode("M");
+        maritalCoding.setDisplay("married");
+        CodeableConcept maritalStat = new CodeableConcept();
+        maritalStat.addCoding(maritalCoding);
+
+        pat.setMaritalStatus(maritalStat);
+
+        // Set Photo
+        File file = new File("src/main/resources/PAT_FOTO.png");
+        byte[] bytes = new byte[(int)file.length()];
+        try {
+            FileInputStream fileInputStreamReader = new FileInputStream(file);
+            fileInputStreamReader.read(bytes);
+        } catch(
+                IOException e)    {
+            System.out.println(e);
+        }
+
+        Attachment photo = new Attachment();
+        photo.setData(bytes);
+        pat.addPhoto(photo);
+
+        String encoded = this.ctx.newXmlParser().setPrettyPrint(true).encodeResourceToString(pat);
+        storeMsg(encoded, "Patient.xml");
+
+        encoded = this.ctx.newJsonParser().setPrettyPrint(true).encodeResourceToString(pat);
+        storeMsg(encoded, "Patient.json");
+    }
+
+    public static void storeMsg(String msg, String name) {
+
+        File dir = new File("output");
+        if(!dir.exists())   {
+            dir.mkdir();
+        }
+        File file = new File("output/" + name);
+
+        try {
+            FileWriter writer = new FileWriter(file ,false);
+            writer.write(msg);
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ src/main/java/utils.java
@@ -0,0 +1,180 @@
+import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.parser.IParser;
+import ca.uhn.fhir.rest.client.api.IGenericClient;
+import org.hl7.fhir.instance.model.api.IBaseResource;
+import org.hl7.fhir.r4.model.*;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class utils {
+
+    /*
+    Method which returns all Patient Names
+     */
+    public static void getAllNames(FhirContext ctx, String outputPath){
+
+        IGenericClient client = ctx.newRestfulGenericClient("https://funke.imi.uni-luebeck.de/public/fhir");
+
+        //create Bundle by searching for Patients
+        Bundle results = client
+                .search()
+                .forResource(Patient.class)
+                .returnBundle(Bundle.class)
+                .execute();
+
+
+        //Go through bundle and write name for each patient to list
+        ArrayList<String> allPatientNames = new ArrayList<>();
+
+        for(Bundle.BundleEntryComponent tmp:results.getEntry()) {
+            Patient currentPatient = (Patient) tmp.getResource();
+            allPatientNames.add(currentPatient.getName().get(0).getNameAsSingleString());
+        }
+
+        //Write Names to txt file
+        try (FileWriter fileTXT = new FileWriter(outputPath + "allPatientNamesList.txt")) {
+
+            for (String s : allPatientNames) {
+                fileTXT.write(s + "\n");
+            }
+            fileTXT.flush();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public static void anotherTryForHospital(FhirContext ctx, String outputPath)    {
+
+        // Create an organization
+        Organization org = new Organization();
+        org.setId("Organization/65546");
+        org.setName("Test Organization");
+
+        Organization station = new Organization();
+        station.setId("Station/1234");
+        station.setName("Geburtsstation");
+
+        // Create a patient
+        Patient patient = new Patient();
+        patient.setId("Patient/1333");
+        patient.addIdentifier().setSystem("urn:mrns").setValue("253345");
+        patient.getManagingOrganization().setResource(org);
+
+        // Here we return only the patient object, which has links to other resources
+        List<IBaseResource> retVal = new ArrayList<IBaseResource>();
+        retVal.add(patient);
+
+        IParser parser =  ctx.newJsonParser();
+        parser.setPrettyPrint(true);
+
+        String encode = parser.encodeResourceToString(retVal.get(0));
+        System.out.println(encode);
+
+
+
+    }
+
+
+    public static void makeWholeHospital(FhirContext ctx, String outputPath){
+
+        // Set up station and reference
+        Organization station = new Organization();
+        station.setName("Geburtsstation");
+        station.addIdentifier().setValue(String.valueOf(new Random().nextInt(10000)));
+        Reference refToStat = new Reference();
+        refToStat.setId(station.getId());
+        refToStat.setResource(station);
+
+
+        //Set up hospital and reference
+        Organization hospital = new Organization();
+        Address adressOfHospital = new Address();
+        adressOfHospital.setCity("Lübeck");
+        hospital.setName("MIO");
+        hospital.addAddress(adressOfHospital);
+        hospital.addIdentifier().setValue(String.valueOf(new Random().nextInt(10000)));
+        Reference refToHosp = new Reference();
+        refToHosp.setId(hospital.getId());
+
+        // link staton and hospital
+        hospital.getEndpoint().add(refToStat); //Station is Part of hospital.
+        //station.getPartOf().setResource(hospital);
+
+        //create Doctor
+        Practitioner doc = new Practitioner();
+        HumanName docName = new HumanName();
+        docName.setUse(HumanName.NameUse.OFFICIAL);
+        docName.setFamily("Wackel");
+        docName.addGiven("Peter");
+        doc.addName(docName);
+        PractitionerRole roleOfDoc = new PractitionerRole();
+        CodeableConcept conceptDoc = new CodeableConcept();
+        conceptDoc.addCoding().setCode("doctor").setSystem("http://terminology.hl7.org/CodeSystem/practitioner-role").setDisplay("Doctor");
+        roleOfDoc.addCode(conceptDoc);
+        roleOfDoc.setOrganization(refToStat);
+        roleOfDoc.setPractitionerTarget(doc);
+        roleOfDoc.setOrganization(refToStat); //RoleofDoc(Doc) is Part of station.
+        Reference refDocStat = new Reference();
+        refDocStat.setResource(doc);
+
+        // link doc to station
+        station.getEndpoint().add(refDocStat);
+
+        //create Nurse
+        Practitioner sis = new Practitioner();
+        HumanName sisName =  new HumanName();
+        sisName.setUse(HumanName.NameUse.OFFICIAL);
+        sisName.setFamily("Wackelina");
+        sisName.addGiven("Petra");
+        sis.addName(sisName);
+        PractitionerRole roleOfSis = new PractitionerRole();
+        CodeableConcept conceptSis = new CodeableConcept();
+        conceptSis.addCoding().setCode("nurse").setSystem("http://terminology.hl7.org/CodeSystem/practitioner-role").setDisplay("Nurse");
+        roleOfSis.addCode(conceptSis);
+        roleOfSis.setOrganization(refToStat);
+        roleOfSis.setPractitionerTarget(sis);
+        roleOfSis.setOrganization(refToStat); //RoleofSis(Sis) is Part of station.
+        Reference refSisStat = new Reference();
+        refSisStat.setResource(sis);
+
+        // link sis to station
+        station.getEndpoint().add(refSisStat);
+
+        IParser parser =  ctx.newJsonParser();
+        parser.setPrettyPrint(true);
+
+        String encode = parser.encodeResourceToString(hospital);
+        System.out.println(encode);
+
+        //Write all to JSON file.
+        //List<IBaseResource> retVal = new ArrayList<IBaseResource>();
+        // retVal.add(hospital);
+
+//        List<IBaseResource> retVal = new ArrayList<IBaseResource>();
+//        retVal.add(hospital);
+//
+//        for(Resource r :retVal) {
+//            System.out.println();
+//        }
+
+        String outputJSON = ctx.newJsonParser().setPrettyPrint(true).encodeResourceToString(hospital);
+        try (FileWriter fileJSON = new FileWriter(outputPath + "ALL.json")) {
+
+            fileJSON.write(outputJSON);
+            fileJSON.flush();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
+
