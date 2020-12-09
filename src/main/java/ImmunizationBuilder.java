58aa1e1ac48dfc67d33e2c17ee2a9638d456f3e2
Konrad <konrad.vonkuegelgen@student.uni-luebeck.de>
2020-12-09 21:15:23 +0100

    standard immunizations

--- src/main/java/ImmunizationBuilder.java
+++ src/main/java/ImmunizationBuilder.java
@@ -26,8 +26,8 @@ public class ImmunizationBuilder {
      *
      * @param conceptVaccineCode
      */
-    public Immunization newImmunization(CodeableConcept conceptVaccineCode,
-                                        String occurrenceDate,
+    public Immunization newImmunization(String occurrenceDate,
+                                        CodeableConcept conceptVaccineCode,
                                         PractitionerRole doctor,
                                         String lotNumber,
                                         CodeableConcept targetDisease,
@@ -87,8 +87,8 @@ public class ImmunizationBuilder {
 
         for (ArrayList<String> sublist : immuInfo) {
             immu = newImmunization(
-                    new CodeableConcept(new Coding(sublist.get(1), sublist.get(2), sublist.get(3))),
                     sublist.get(0),
+                    new CodeableConcept(new Coding(sublist.get(1), sublist.get(2), sublist.get(3))),
                     this.doctor_roles.get(new Random().nextInt(this.doctor_roles.size())),
                     sublist.get(4),
                     new CodeableConcept(new Coding(sublist.get(5), sublist.get(6), sublist.get(7))),
@@ -111,6 +111,10 @@ public class ImmunizationBuilder {
         MethodOutcome methodOutcome;
 
         ArrayList<ArrayList<String>> immuInfo = new ArrayList<>();
+        /*
+         * Kombi Impfung Priorix Measles, Mumps, Rubella
+         */
+        immuInfo.add(new ArrayList<String>() {{
         immuInfo.add(new ArrayList<String>() {{
             add("1993-07-14");
             // description of vaccine
@@ -118,7 +122,7 @@ public class ImmunizationBuilder {
             add("MMRSKB");
             add("Priorix");
             // chargen nummer
-            add("690010PD");
+            add("A69CD425A");
             // target disease
             add("http://snomed.info/sct");
             add("36989005");
@@ -126,6 +130,99 @@ public class ImmunizationBuilder {
             // doseNumber
             add("1");
         }});
+            add("1993-07-14");
+            // description of vaccine
+            add("urn:oid:1.2.36.1.2001.1005.17");
+            add("MMRSKB");
+            add("Priorix");
+            // chargen nummer
+            add("A69CD425A");
+            // target disease
+            add("http://snomed.info/sct");
+            add("14189004");
+            add("Measles");
+            // doseNumber
+            add("1");
+        }});
+        immuInfo.add(new ArrayList<String>() {{
+            add("1993-07-14");
+            // description of vaccine
+            add("urn:oid:1.2.36.1.2001.1005.17");
+            add("MMRSKB");
+            add("Priorix");
+            // chargen nummer
+            add("A69CD425A");
+            // target disease
+            add("http://snomed.info/sct");
+            add("36653000");
+            add("Rubella");
+            // doseNumber
+            add("1");
+        }});
+        /*
+         * Kombi Impfung Tetanus, Diphterie, Poliomyelitis, Pertussis
+         */
+        immuInfo.add(new ArrayList<String>() {{
+            add("1993-07-23");
+            // description of vaccine
+            add("http://hl7.org/fhir/sid/cvx");
+            add("115");
+            add("Tdap");
+            // chargen nummer
+            add("J0021-1");
+            // target disease
+            add("http://snomed.info/sct");
+            add("76902006");
+            add("Tetanus");
+            // doseNumber
+            add("1");
+        }});
+        immuInfo.add(new ArrayList<String>() {{
+            add("1993-07-23");
+            // description of vaccine
+            add("http://hl7.org/fhir/sid/cvx");
+            add("115");
+            add("Tdap");
+            // chargen nummer
+            add("J0021-1");
+            // target disease
+            add("http://snomed.info/sct");
+            add("397430003");
+            add("Diphtheria due to Corynebacterium diphtheriae");
+            // doseNumber
+            add("1");
+        }});
+        immuInfo.add(new ArrayList<String>() {{
+            add("1993-07-23");
+            // description of vaccine
+            add("http://hl7.org/fhir/sid/cvx");
+            add("115");
+            add("Tdap");
+            // chargen nummer
+            add("J0021-1");
+            // target disease
+            add("http://snomed.info/sct");
+            add("398102009");
+            add("Acute poliomyelitis");
+            // doseNumber
+            add("1");
+        }});
+        immuInfo.add(new ArrayList<String>() {{
+            add("1993-07-23");
+            // description of vaccine
+            add("http://hl7.org/fhir/sid/cvx");
+            add("115");
+            add("Tdap");
+            // chargen nummer
+            add("J0021-1");
+            // target disease
+            add("http://snomed.info/sct");
+            add("27836007");
+            add("Pertussis");
+            // doseNumber
+            add("1");
+        }});
+
 
         Composition.SectionComponent tmp = new Composition.SectionComponent();
         tmp.setTitle("Vaccinations");
@@ -133,8 +230,8 @@ public class ImmunizationBuilder {
 
         for (ArrayList<String> sublist : immuInfo) {
             immu = newImmunization(
-                    new CodeableConcept(new Coding(sublist.get(1), sublist.get(2), sublist.get(3))),
                     sublist.get(0),
+                    new CodeableConcept(new Coding(sublist.get(1), sublist.get(2), sublist.get(3))),
                     this.doctor_roles.get(new Random().nextInt(this.doctor_roles.size())),
                     sublist.get(4),
                     new CodeableConcept(new Coding(sublist.get(5), sublist.get(6), sublist.get(7))),
@@ -164,7 +261,7 @@ public class ImmunizationBuilder {
             // target disease
             add("http://snomed.info/sct");
             add("6142004");
-            add(" Influenza (disorder)");
+            add("Influenza (disorder)");
             // doseNumber
             add("1");
         }});
@@ -175,8 +272,8 @@ public class ImmunizationBuilder {
 
         for (ArrayList<String> sublist : immuInfo) {
             immu = newImmunization(
-                    new CodeableConcept(new Coding(sublist.get(1), sublist.get(2), sublist.get(3))),
                     sublist.get(0),
+                    new CodeableConcept(new Coding(sublist.get(1), sublist.get(2), sublist.get(3))),
                     this.doctor_roles.get(new Random().nextInt(this.doctor_roles.size())),
                     sublist.get(4),
                     new CodeableConcept(new Coding(sublist.get(5), sublist.get(6), sublist.get(7))),
